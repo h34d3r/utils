e8d6ebc063cec136f7c9c7b898763d379c0084a0
Jakub Tarchalski <leluckyman525@gmail.com>
2018-07-22 17:23:44 +0100

    slight error fix in wifi starting script

--- internet/wifi.sh
+++ internet/wifi.sh
@@ -4,5 +4,5 @@ sudo killall dhcpcd
 echo "choose wifi network"
 echo "options: dad	mum	hotspot"
 read wifi
-sudo wpa_supplicant -B -i wlp1s0 -c internet-$wifi.cfg
+sudo wpa_supplicant -B -i wlp1s0 -c ~/utils/internet/internet-$wifi.cfg
 sudo dhcpcd wlp1s0
