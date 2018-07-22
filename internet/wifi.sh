1b0d2db9a79879aa2c8a595e2b26b55fc3cd49d1
Jakub Tarchalski <leluckyman525@gmail.com>
2018-07-22 23:36:38 +0100

    streamlined wifi starting file and made a script to add networks to my wifi system

--- internet/wifi.sh
+++ internet/wifi.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 sudo killall wpa_supplicant
 sudo killall dhcpcd
+LIST=$(cat NAMELIST)
 echo "choose wifi network"
-echo "options: dad	mum	hotspot"
-read wifi
-sudo wpa_supplicant -B -i wlp1s0 -c ~/utils/internet/internet-$wifi.cfg
+echo "options: $LIST"
+read NAME
+sudo wpa_supplicant -B -i wlp1s0 -c ~/utils/internet/internet-$NAME.cfg
 sudo dhcpcd wlp1s0
---
+++ internet/wifi_add.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "what is the network's SSID?"
+read SSID
+echo "what is the network's PASSWORD?"
+read PASS
+echo "what do you want to call this network?"
+read NAME
+
+echo "wpa_passphrase '$SSID''$PASS'" > ~/utils/internet/internet-$NAME.cfg
+echo "$NAME" >> ~/utils/internet/NAMELIST
